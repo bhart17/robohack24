7303c4691a0c17807abe410c1081eeb20d68fa7f
Kit <74719913+ked4797@users.noreply.github.com>
2024-05-05 00:28:10 +0100

    Add files via upload

--- robot.py
+++ robot.py
@@ -5,39 +5,57 @@ import time
 
 robot = Robot()
 
+motors = robot.motor_board.motors
+
 my_arduino = robot.arduino
+
+def stop():
+    motors[0].power = 0
+    motors[1].power = 0
+
+def drive(power, duration):
+    motors[0].power = power
+    motors[1].power = -power
+    robot.sleep(duration)
+    stop()
+
+def turn(power, duration):
+    motors[0].power = power
+    motors[1].power = power
+    robot.sleep(duration)
+    stop()
+
+
 
 #high on white low on black
 
 #left line following sensor
-A0 = AnalogPins.A0
+rightSensor = AnalogPins.A0
 #right line following sensor
-A1 = AnalogPins.A1
+leftSensor = AnalogPins.A1
 
-my_arduino.pins[A0].mode = GPIOPinMode.INPUT
-my_arduino.pins[A1].mode = GPIOPinMode.INPUT
+my_arduino.pins[rightSensor].mode = GPIOPinMode.INPUT
+my_arduino.pins[leftSensor].mode = GPIOPinMode.INPUT
 
-threshold = 0.15
+threshold = 0.1
 
 while True:
-    left_reading = my_arduino.pins[A0].analog_value
-    right_reading = my_arduino.pins[A1].analog_value
+    left_reading = my_arduino.pins[leftSensor].analog_value
+    right_reading = my_arduino.pins[rightSensor].analog_value / 10
+    
     print("Left:")
     print(left_reading)
     print("Right:")
     print(right_reading)
 
-    if (left > threshold):
+    if (left_reading > threshold):
         #turn right a bit
-    else if (right > threshold):
+        turn(-0.1, 0.01)
+    elif (right_reading > threshold):
         #turn left a bit
+        turn(0.1, 0.01)
     else:
         #keep going straight
+        drive(0.2, 0.3)
     
-    time.sleep(0.001)
-    
-
-
-
-
-
+    #time.sleep(0.001)
